2ee99937abbd0cdfcecc37843f064377c4345d21
Mona Lisagit config --global user.email <stedy6@hotmail.com>
2018-06-24 21:08:24 +0200

    first commit, just started on MB block

---
+++ SCDlibSharp/SCDlibSharp/MyClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SCDlibSharp
+{
+	public class functionalTemplates
+	{
+		public MB ()
+		*/ The MB function templae shall be used for automatic monitoring(alrming), display and latching of binary process variable*/
+		
+		{
+			//inputs
+		public bolean X;
+		public bolean XF;
+		public bolean RX;
+		public bolean FB;	
+		public bolean FU;
+			//Outputs
+		public bolean Y;
+		public bolean YF;
+		public bolean BB;
+		public bolean BU;	
+		public bolean BX;
+			//parmaters
+			public bolean invet_input;
+			public bolean latced_output; 
+			public int time_delay_alarm; // in seconds
+		}
+
+	}
+}
+
